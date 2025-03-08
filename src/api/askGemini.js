d0441009497aff98c91f82a6fb86ed312a5a0d32
Dayoung Kim <dayoungkim1027@gmail.com>
2025-03-08 18:34:20 -0500

    will this work

--- src/api/askGemini.js
+++ src/api/askGemini.js
@@ -2,7 +2,7 @@ export const apiUrl = process.env.BACKEND_URL;
 export const apiKey = process.env.API_KEY;
 
 export default async function AskGemini(prompt) {
-	const url = 'https://survey-backend-h3b93sh56-dayoungkim1027s-projects.vercel.app/ask/gemini';
+	const url = 'https://survey-backend-h3b93sh56-dayoungkim1027s-projects.vercel.app';
 	const data = {
 		text: prompt
 	}
@@ -11,13 +11,13 @@ export default async function AskGemini(prompt) {
 		headers: {
 			'Content-Type': 'application/json',
 			'Authorization': apiKey,
-			'Access-Control-Allow-Origin': '*'
+			'Access-Control-Allow-Origin': url
 		},
 		body: JSON.stringify(data)
 	};
 	
 	try {
-		const response = await fetch(url, options);
+		const response = await fetch(`${url}/ask/gemini`, options);
 		return await response.text();
 	}	catch (e) {
 		console.log('error')
