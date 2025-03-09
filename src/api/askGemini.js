51c44874e99e78dce1755f454d5601c7a5b3987e
Dayoung Kim <dayoungkim1027@gmail.com>
2025-03-08 22:19:38 -0500

    will this work

--- src/api/askGemini.js
+++ src/api/askGemini.js
@@ -1,24 +1,20 @@
+import axios from 'axios';
 export const apiUrl = process.env.BACKEND_URL;
 export const apiKey = process.env.API_KEY;
 
 export default async function AskGemini(prompt) {
-	const url = 'https://survey-backend-six.vercel.app';
+	const url = 'https://survey-backend-six.vercel.app/ask/gemini';
 	const data = {
 		text: prompt
 	}
-	const options = {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			'Authorization': apiKey,
-			'Access-Control-Allow-Origin': '*',
-		},
-		body: JSON.stringify(data),
-		// mode: 'cors'
+	const headers = {
+		'Content-Type': 'application/json',
+		'Authorization': apiKey
 	};
 	
 	try {
-		const response = await fetch('https://survey-backend-six.vercel.app/ask/gemini', options);
+		const response = await axios.post(url, data, headers);
+		// const response = await fetch('https://survey-backend-six.vercel.app/ask/gemini', options);
 		if (!response.ok) {
 			throw new Error(`HTTP error! Status: ${response.status}`);
 		}
