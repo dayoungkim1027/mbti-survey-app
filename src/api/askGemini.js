02508d39c54108c4c9bd67e69cd17f23c3c71ef0
Dayoung Kim <dayoungkim1027@gmail.com>
2025-03-08 23:00:50 -0500

    lets not use backend

--- src/api/askGemini.js
+++ src/api/askGemini.js
@@ -1,24 +1,18 @@
-import axios from 'axios';
-export const apiUrl = process.env.BACKEND_URL;
-export const apiKey = process.env.API_KEY;
+import { GoogleGenerativeAI } from '@google/generative-ai';
+// import axios from 'axios';
+// export const apiUrl = process.env.BACKEND_URL;
+// export const apiKey = process.env.API_KEY;
+const geminiApiKey = process.env.REACT_APP_GEMINI_API_KEY;
+const modelName = process.env.REACT_APP_MODEL_NAME;
+
 
 export default async function AskGemini(prompt) {
-	const url = 'https://survey-backend-six.vercel.app/ask/gemini';
-	const data = {
-		text: prompt
-	}
-	const headers = {
-		'Content-Type': 'application/json',
-		'Authorization': apiKey
-	};
-	
 	try {
-		const response = await axios.post(url, data, headers);
-		// const response = await fetch('https://survey-backend-six.vercel.app/ask/gemini', options);
-		if (!response.ok) {
-			throw new Error(`HTTP error! Status: ${response.status}`);
-		}
-		return await response.text();
+		const genAI = new GoogleGenerativeAI(geminiApiKey);
+		const model = genAI.getGenerativeModel({ model: modelName });
+		let result = await model.generateContent(prompt);
+		console.log('.result.response.text(): ', result.response.text());
+		return await result.response.text();
 	}	catch (e) {
 		console.log('error')
 	}
