1dcfd610ef916999599d76e394ba62e1e8a24298
Dayoung Kim <dayoungkim1027@gmail.com>
2025-03-08 20:14:38 -0500

    this too

--- src/api/askGemini.js
+++ src/api/askGemini.js
@@ -3,20 +3,20 @@ export const apiKey = process.env.API_KEY;
 
 export default async function AskGemini(prompt) {
 	const url = 'https://survey-backend-h3b93sh56-dayoungkim1027s-projects.vercel.app';
-	const data = {
-		text: prompt
-	}
+	// const data = {
+	// 	text: prompt
+	// }
 	const options = {
-		method: 'POST',
+		method: 'GET',
 		headers: {
 			'Content-Type': 'application/json',
 			'Authorization': apiKey
 		},
-		body: JSON.stringify(data)
+		// body: JSON.stringify(data)
 	};
 	
 	try {
-		const response = await fetch(`${url}/ask/gemini`, options);
+		const response = await fetch(`${url}/ask/gemini/${prompt}`, options);
 		return await response.text();
 	}	catch (e) {
 		console.log('error')
